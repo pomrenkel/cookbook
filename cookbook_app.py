bb31f7212351bdf5b43bc24f4bc944506968670d
pomrenkel <53022111+pomrenkel@users.noreply.github.com>
2021-03-19 15:48:51 -0400

    Added loadConfig method to read in YAML config file

--- cookbook_app.py
+++ cookbook_app.py
@@ -1,3 +1,19 @@
+import yaml
+
+
+def loadConfig(file_path):
+    """Returns config dict object from given filepath"""
+    try:
+        with open(file_path, "r") as cfg_file:
+            cfg = yaml.safe_load(cfg_file)
+    except Exception as e:
+        print("Could not find file path: " + file_path)
+    else:
+        print("*** Config file found. ***")
+        return cfg
+
+
+
 
 if __name__ == "__main__":
     pass
