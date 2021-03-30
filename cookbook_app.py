eafb9dccd8f1b89403e04f2e655721e7bfd86cf1
pomrenkel <53022111+pomrenkel@users.noreply.github.com>
2021-03-30 11:21:51 -0400

    Added basic input cleaning/command validation to processInput method

--- cookbook_app.py
+++ cookbook_app.py
@@ -1,3 +1,5 @@
+import os
+import sys
 import yaml
 import mysql.connector
 
@@ -10,11 +12,44 @@ class CBController:
         self._MyView = CBView()
 
     def test(self):
+        """Test method for running and printing output of a simple SELECT * in the test db"""
         self._MyModel.formatQuery()
         self._MyModel.execQuery()
 
-    def processInput(self, *args):
-        options = {}
+    def processInput(self):  # TODO: cleaning input, parsing for optionals, and passing to correct command
+        raw_input = input("Waiting for input: ")
+        result_list = raw_input.strip().lower().split()
+
+        # Verification portion of the processing - raising exceptions here if errors
+        if result_list[0] not in COMMANDS:
+            raise KeyError("Please enter a valid command.")
+        else:
+            command = COMMANDS[result_list[0]]
+
+
+
+        #Analyzing list input for validity
+
+        #If correct, passing list input to correct command
+
+
+    def eventLoop(self):
+        while True:
+            try:
+                self.processInput()
+            except KeyError:
+                print("Please enter a valid command")
+            return
+
+            # user_in = input("Waiting for input: ").lower()
+            # if user_in == "q":
+            #     print("Exiting")
+            #     return
+            # elif user_in in COMMANDS:
+            #     print(f"You chose {COMMANDS[user_in]}")
+            # else:
+            #     print("Please enter a valid command")
+            #     self._MyView.printCommands()
 
 
 class CBModel:
@@ -51,28 +86,33 @@ class CBModel:
 
 class CBView:
     def __init__(self):
-        self.sql_result = None
-        self.formatted_out = None
+        self.sql_result = None # Raw response
+        self.formatted_out = None # Printing out just what needed
 
     def printWelcome(self):
+        """Prints out the program initial welcome message"""
         print("\nWelcome to the cookbook!\n")
         self.printCommands()
 
-    def printCommands(self):
+    @staticmethod
+    def printCommands():
+        """Prints out command keys and their respective command"""
         for k, v in COMMANDS.items():
             print(k + "\t\t\t" + v)
         print()
 
-    def readIn(self, sql_result):
-        # Processing sql results to format for outputting
+    def formatResults(self, sql_result):
+        """Formats raw SQL results and stores raw/formatted of most recent result"""
         pass
 
     def printResults(self):
+        """Displays formatted SQL results to user"""
         # printing out the formatted results as specified
         pass
 
 
 if __name__ == "__main__":
-    myController = CBController("config.yaml")
+    myController = CBController("config.yaml")  # TODO: Add config file selection as first separate step
     myController._MyView.printWelcome()
+    myController.eventLoop()
     myController.test()
